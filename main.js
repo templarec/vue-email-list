733594759d20b7940ad8f5ec82f7a6684ddfb151
Lorenzo Bernini <templarec@gmail.com>
2021-04-08 17:33:52 +0200

    Esercizio completato + bonus

--- main.js
+++ main.js
@@ -3,7 +3,21 @@
 });*/
 var app = new Vue({
 	el: '#root',
-	data: {},
-	component: {},
-	methods: {}
+	data: {
+		emailArray: [],
+		lunghezza: 10
+	},
+	methods: {
+		generate: function (numero) {
+			for (let i = 0; i < numero ; i++) {
+				this.emailArray = [];
+				$('#lista').html('');
+				axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+					.then( (risposta) => {
+						$('#lista').append(`<li>${risposta.data.response}</li>`)
+						this.emailArray.push(risposta.data.response);
+					});
+			}
+		}
+	}
 });
